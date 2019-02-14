39d35b928c3536db10ab1cb35a82fcd19b2365ac
cryzed <cryzed@googlemail.com>
2019-02-14 20:53:17 +0100

    Some more GUI work

--- traffictoll/gui/mainwindow.py
+++ traffictoll/gui/mainwindow.py
@@ -4,7 +4,27 @@ from PyQt5.QtGui import QStandardItem, QStandardItemModel
 from PyQt5.QtWidgets import QMainWindow
 
 from traffictoll.gui.views.mainwindow import Ui_MainWindow
-from traffictoll.net import get_net_connections
+
+
+class LeafSortFilterProxyModel(QSortFilterProxyModel):
+    def filterAcceptsRow(self, row, parent):
+        if super().filterAcceptsRow(row, parent):
+            return True
+
+        # Check if any ancestors accepts the filter
+        while parent.isValid():
+            if super().filterAcceptsRow(parent.row(), parent.parent()):
+                return True
+            parent = parent.parent()
+
+        # Check if any descendants accepts the filter
+        model = self.sourceModel()
+        index = model.index(row, 0, parent)
+        for row in range(model.rowCount(index)):
+            if super().filterAcceptsRow(row, index):
+                return True
+
+        return False
 
 
 class MainWindow(QMainWindow):
@@ -16,12 +36,12 @@ class MainWindow(QMainWindow):
         self.processes = QStandardItemModel(0, 5)
         self.processes.setHeaderData(0, Qt.Horizontal, 'Name', Qt.DisplayRole)
         self.processes.setHeaderData(1, Qt.Horizontal, 'PID', Qt.DisplayRole)
-        self.processes.setHeaderData(2, Qt.Horizontal, 'Executable', Qt.DisplayRole)
-        self.processes.setHeaderData(3, Qt.Horizontal, 'CMD', Qt.DisplayRole)
-        self.processes.setHeaderData(4, Qt.Horizontal, 'Networked', Qt.DisplayRole)
+        self.processes.setHeaderData(2, Qt.Horizontal, 'Command', Qt.DisplayRole)
+        self.processes.setHeaderData(3, Qt.Horizontal, 'Download Limit', Qt.DisplayRole)
+        self.processes.setHeaderData(4, Qt.Horizontal, 'Upload Limit', Qt.DisplayRole)
 
         # TODO: Subclass QSortFilterProxyModel to match on any child
-        self.processes_filter = QSortFilterProxyModel(self.processes)
+        self.processes_filter = LeafSortFilterProxyModel(self.processes)
         self.processes_filter.setSourceModel(self.processes)
         self.processes_filter.setFilterKeyColumn(-1)
         self.processes_filter.setFilterCaseSensitivity(Qt.CaseInsensitive)
@@ -32,11 +52,10 @@ class MainWindow(QMainWindow):
         self.ui.processes.setModel(self.processes_filter)
         self.ui.processes.setSelectionModel(self.processes_selection)
 
-        self.networked_pids = {connection.pid for connection in get_net_connections()}
         root = psutil.Process(1)
-        self._insert_tree(root)
+        self._insert_tree(root, self.processes)
 
-    def _insert_tree(self, tree, parent=None):
+    def _insert_tree(self, tree, parent):
         root_item = QStandardItem(tree.name())
         root_item.setData(tree, Qt.UserRole)
         for child in tree.children():
@@ -44,15 +63,14 @@ class MainWindow(QMainWindow):
 
         columns = [
             QStandardItem(str(tree.pid)),
-            QStandardItem(tree.exe()),
             QStandardItem(' '.join(tree.cmdline())),
-            QStandardItem('Yes' if tree.pid in self.networked_pids else 'No')]
-        (parent or self.processes).appendRow([root_item, *columns])
+            QStandardItem(),
+            QStandardItem()]
+        parent.appendRow([root_item, *columns])
 
     @pyqtSlot(str)
     def on_search_textChanged(self, text):
         self.processes_filter.setFilterFixedString(text)
-        print(self.processes_selection.selectedRows())
 
     def _on_selection_changed(self, before, now):
         pids = []
@@ -60,5 +78,3 @@ class MainWindow(QMainWindow):
             process = selected.data(Qt.UserRole)
             pids.append(process.pid)
             pids.extend(c.pid for c in process.children(recursive=True))
-
-        print(pids)
